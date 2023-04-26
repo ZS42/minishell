6f84625a051a62b677b54bd3cef0b18f853942a9
Mohammed Ruhan Goltay <mgoltay@lab3r5s1.42abudhabi.ae>
2023-04-26 19:35:09 +0400

    sub

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: mgoltay <mgoltay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:24:02 by zsyyida           #+#    #+#             */
-/*   Updated: 2023/04/25 17:23:32 by mgoltay          ###   ########.fr       */
+/*   Updated: 2023/04/26 19:34:18 by mgoltay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,31 @@
 // store exit status in global int to be able to aces it again
 // also to set it when not exiting like when too many arguments
 // check it with echo $? which should print exit status number
+
+int	fits_in_long_long(char *str)
+{
+	long long	out;
+	int			c;
+
+	if (ft_strlen(str) > 20)
+		return (0);
+	if (ft_strncmp(str, "-9223372036854775808", 21) == 0)
+		return (1);
+	out = 0;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		c = *str - '0';
+		if (out > (LLONG_MAX - c) / 10)
+			return (0);
+		out = out * 10 + c;
+		str++;
+	}
+	return (1);
+}
 
 int	ft_atoi_exit(const char *str)
 {
@@ -75,7 +100,7 @@ void	ft_exit(t_shell *shell, t_list_cmd *l)
 		g_exit_status = 1;
 		return ;
 	}
-	else if (l->cmd[1] && ft_check_numeric(l->cmd[1]) == 1)
+	else if (l->cmd[1] && (ft_check_numeric(l->cmd[1]) || !fits_in_long_long(l->cmd[1])))
 	{
 		ft_putstr_fd("reach", 2);
 		ft_putstr_fd("ruhan_zahra_shell: exit: ", 2);
